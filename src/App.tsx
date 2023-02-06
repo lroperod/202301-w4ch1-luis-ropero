7cd9269dd6c51f16cda137e72df5755c34bb72d1
luis <luisrym@gmail.com>
2023-02-06 21:42:58 +0100

    add component card to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card/Gentleman";
+import gentlemanArray from "./Data/data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Card></Card>
-      </header>
+      <header className="App-header"></header>
+      {gentlemanArray.map(gentleman => (
+        <Card gentleman={gentleman} />
+      ))}
     </div>
   );
 }
